b5fd6d0916d729b481a29b72158e9b388a4f6664
Oliver Foster <oliver.foster@kineo.com>
2016-08-01 16:20:45 +0100

    Update

--- ntract-registry.cmd
+++ ntract-registry.cmd
@@ -1,7 +1,7 @@
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\plugins\interact-cli\index.js" %*
+  "%~dp0\node.exe"  "%~dp0\plugins\ntract-cli\index.js" %*
 ) ELSE (
   @SETLOCAL
   @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\plugins\interact-cli\index.js" %*
+  node  "%~dp0\plugins\ntract-cli\index.js" %*
 )
